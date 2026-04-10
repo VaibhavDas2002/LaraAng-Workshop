57d8b3f46866d095af846ed3358c4311a8341f7c
Vaibhav Das <dasvaibhav2002@gmail.com>
2026-04-10 12:52:44 +0530

    wip

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Register } from './components/register/register';
 import { ProductList } from './components/product-list/product-list';
 import { ProductForm } from './components/product-form/product-form';
 import { authGuard } from './guards/auth.guard';
+import { Skin } from './components/skin/skin';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'products', component: ProductList, canActivate: [authGuard] },
   { path: 'products/create', component: ProductForm, canActivate: [authGuard] },
   { path: 'products/edit/:id', component: ProductForm, canActivate: [authGuard] },
-  { path: '**', redirectTo: 'login' }
+  { path: 'skin', component: Skin, canActivate: [authGuard] },
+  { path: '**', redirectTo: 'login' },
 ];
